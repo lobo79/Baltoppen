59f35214645ee0ecb6fb586067a130b8ea3bd618
lobo79 <39367037+lobo79@users.noreply.github.com>
2019-06-19 09:04:43 +0200

    Nye projekt mappe

---
+++ Baltoppen/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Baltoppen.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Baltoppen.Controllers
+{
+    public class HomeController : Controller
+    {
+        // private BaltoppenEntities db = new BaltoppenEntities();
+        private BaltoppenEntities1 db = new BaltoppenEntities1();
+
+        public ActionResult Index()
+        {
+            return View(db.Movies.ToList());
+        }
+
+        
+
+        public ActionResult ComingMovies()
+        {
+            ViewBag.Message = "Kommende film.";
+
+            return View();
+        }
+
+        public ActionResult BuyGiftcard()
+        {
+            ViewBag.Message = "Køb et gavekort.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Baltoppen/Controllers/TicketOrderController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Baltoppen.Models;
+
+namespace Baltoppen.Controllers
+{
+    public class ScreeningsController : Controller
+    {
+        private BaltoppenEntities1 db = new BaltoppenEntities1();
+
+        // GET: Screenings
+        public ActionResult Index()
+        {
+            return View(db.Screenings.ToList());
+        }
+
+        // GET: Screenings/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Screenings screenings = db.Screenings.Find(id);
+            if (screenings == null)
+            {
+                return HttpNotFound();
+            }
+            return View(screenings);
+        }
+
+        // GET: Screenings/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Screenings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ScreeningId,ScreeningStart,FkMovieId,FkTheaterId")] Screenings screenings)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Screenings.Add(screenings);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(screenings);
+        }
+
+        // GET: Screenings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Screenings screenings = db.Screenings.Find(id);
+            if (screenings == null)
+            {
+                return HttpNotFound();
+            }
+            return View(screenings);
+        }
+
+        // POST: Screenings/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ScreeningId,ScreeningStart,FkMovieId,FkTheaterId")] Screenings screenings)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(screenings).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(screenings);
+        }
+
+        // GET: Screenings/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Screenings screenings = db.Screenings.Find(id);
+            if (screenings == null)
+            {
+                return HttpNotFound();
+            }
+            return View(screenings);
+        }
+
+        // POST: Screenings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Screenings screenings = db.Screenings.Find(id);
+            db.Screenings.Remove(screenings);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
